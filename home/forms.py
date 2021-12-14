d318f793342926385376859aa4a360e638c69021
iamkish0re <kishoresreedhar1999@gmail.com>
2021-12-14 22:28:14 +0530

    form changes

--- home/forms.py
+++ home/forms.py
@@ -11,9 +11,9 @@ class UpdatePostForm(forms.ModelForm):
         model = BlogPost
         fields = ('title', 'slug', 'content', 'image')
         widgets = {
-            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Title of the Blog'}),
-            'slug': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Copy the title with no space and a hyphen in between'}),
-            'content': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Content of the Blog'}),
+            'title': forms.TextInput(attrs={'class':'form-control',}),
+            'slug': forms.TextInput(attrs={'class':'form-control', }),
+            'content': forms.Textarea(attrs={'class':'form-control', }),
         }
 class BlogPostForm(forms.ModelForm):
     class Meta:
