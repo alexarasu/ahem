14bc333a251fb07eb8d4c4864b850316bbf70b30
Alex <alexezhilarasu@gmail.com>
2021-12-17 00:25:00 +0530

    Removed edit_profile

--- home/views.py
+++ home/views.py
@@ -87,24 +87,6 @@ def user_profile(request, myid):
 def Profile(request):
     return render(request, "profile.html")
 
-
-def edit_profile(request):
-    try:
-        profile = request.user.profile
-    except Profile.DoesNotExist:
-        profile = Profile(user=request.user)
-    if request.method == "POST":
-        form = ProfileForm(data=request.POST,
-                           files=request.FILES, instance=profile)
-        if form.is_valid():
-            form.save()
-            alert = True
-            return render(request, "edit_profile.html", {'alert': alert})
-    else:
-        form = ProfileForm(instance=profile)
-    return render(request, "edit_profile.html", {'form': form})
-
-
 def Register(request):
     if request.method == "POST":
         username = request.POST['username']
